a9705478692f466cacd31ad0cd005da9ad37306d
Kevin Caborderie <kevin_caborderie@connect-tech.sncf>
2025-01-31 10:37:43 +0100

    Fix Orka variable names

--- GitLab/scripts/run.sh
+++ GitLab/scripts/run.sh
@@ -14,4 +14,4 @@ vm_ip=${info[0]}
 vm_ssh_port=${info[1]}
 
 ssh -i "$ORKA_SSH_KEY_FILE" \
-  -o ServerAliveInterval=$CUSTOM_ENV_ORKA_SSH_ALIVE_INTERVAL -o ServerAliveCountMax=$CUSTOM_ENV_ORKA_SSH_ALIVE_COUNT_MAX "$ORKA_VM_USER@$vm_ip" -p "$vm_ssh_port" /bin/bash < "${1}"
+  -o ServerAliveInterval=$ORKA_SSH_ALIVE_INTERVAL -o ServerAliveCountMax=$ORKA_SSH_ALIVE_COUNT_MAX "$ORKA_VM_USER@$vm_ip" -p "$vm_ssh_port" /bin/bash < "${1}"
